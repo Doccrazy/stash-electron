ce1b76867e5e2d6644623b8c5c586fbbf7808e5c
Matthias Piepkorn <mpiepk@gmail.com>
2017-09-03 12:49:28 +0200

    move page components

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,8 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
+import App from './pages/App';
+import HomePage from './pages/Home';
 
 export default () => (
   <App>
