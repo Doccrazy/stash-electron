eb2626cbaf94ab63f67f0dcea3a65cb6a7cbd443
Matthias Piepkorn <mpiepk@gmail.com>
2017-11-03 00:25:50 +0100

    fix error handler on windows, optimize trace format

--- app/utils/errorHandler.ts
+++ app/utils/errorHandler.ts
@@ -1,24 +1,40 @@
 import * as fs from 'fs';
+import { URL } from 'url';
 import unhandled from 'electron-unhandled';
 import * as sourceMapSupport from 'source-map-support';
+
+function resolveFileUrl(url: string): string {
+  let decoded = decodeURIComponent(new URL(url).pathname);
+  // on windows, remove the / before the drive letter
+  if (decoded[2] === ':') {
+    decoded = decoded.slice(1);
+  }
+  return decoded;
+}
 
 if (process.env.NODE_ENV === 'production') {
   unhandled();
   sourceMapSupport.install({
     handleUncaughtExceptions: false,
+    overrideRetrieveSourceMap: true,
     // electron passes a file: url, which source-map-support can't handle
-    retrieveFile: (path: string) => {
-      path = path.trim();
-      if (path.startsWith('file://')) {
-        path = path.substr('file://'.length);
-        if (fs.existsSync(path)) {
+    retrieveSourceMap: ((source: string) => {
+      if (source.startsWith('file://')) {
+        const sourceMapUrl = `${source}.map`;
+        const resolved = resolveFileUrl(sourceMapUrl);
+        if (fs.existsSync(resolved)) {
           try {
-            return fs.readFileSync(path, 'utf8');
+            const sourceMapData = fs.readFileSync(resolved, 'utf8');
+            return {
+              url: '',  // do not pass url to keep stack filenames relative
+              map: sourceMapData
+            };
           } catch (er) {
+            // ignore
           }
         }
       }
-      return '';
-    }
+      return null;
+    }) as any
   });
 }
