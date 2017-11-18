3bdac4f5d3ff2ead6f8e468a50b95c83a0053913
Matthias Piepkorn <mpiepk@gmail.com>
2017-11-18 18:03:41 +0100

    fix external file drop

--- app/components/FileDropAcceptor.tsx
+++ app/components/FileDropAcceptor.tsx
@@ -1,25 +1,24 @@
-/* eslint-disable no-param-reassign */
 import * as React from 'react';
+import { NativeTypes } from 'react-dnd-html5-backend';
+import { DropTarget, DropTargetMonitor, ConnectDropTarget } from 'react-dnd';
 
-function acceptFiles(ev: React.DragEvent<HTMLElement>) {
-  if ([...ev.dataTransfer.types].includes('Files')) {
-    console.log('accept');
-    ev.preventDefault();
-    ev.dataTransfer.effectAllowed = 'copy';
-    ev.dataTransfer.dropEffect = 'copy';
-  }
-}
-
-function mapFiles(ev: React.DragEvent<HTMLElement>) {
-  return [...(ev.dataTransfer.files) as any].map((file: File) => file.path);
+function mapFiles(files: File[]) {
+  return files.map((file: File) => file.path);
 }
 
 export interface Props {
   children: any,
+  connectDropTarget?: ConnectDropTarget,
   onDrop: (files: string[]) => void,
   [propName: string]: any;
 }
 
-export default ({ children, onDrop, ...props }: Props) => (<div {...props} onDragEnter={acceptFiles} onDragOver={acceptFiles} onDrop={ev => onDrop(mapFiles(ev))}>
+export default DropTarget(NativeTypes.FILE, {
+  drop(props: Props, monitor: DropTargetMonitor) {
+    props.onDrop(mapFiles((monitor.getItem() as any).files));
+  }
+}, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget()
+}))(({ children, onDrop, connectDropTarget, ...props }: Props) => connectDropTarget!(<div {...props}>
   {children}
-</div>);
+</div>));
