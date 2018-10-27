7d45ffeb79ed458fb11f6c5e4759135a0e46c04e
Matthias Piepkorn <mpiepk@gmail.com>
2018-10-27 16:30:53 +0200

    upgrade to immutable 4 rc11 (fixes Set.map)

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,6 @@ import { configureStore, history } from './store/configureStore';
 import { load as loadSettings } from './actions/settings';
 import installLinkHandler from './store/stashLinkHandler';
 import registerHotkeys from './store/hotkeyHandlers';
-import './utils/immutable-set-map-patch.js';
 import './utils/sshpk-key-deriv-patch.js';
 import './app.global.scss';
 import setupInactivityLock from './store/inactivityLock';
