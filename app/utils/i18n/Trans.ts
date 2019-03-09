791e849857588f75c79b6fdc118d13e5a7ffed44
Matthias Piepkorn <mpiepk@gmail.com>
2019-03-10 00:43:39 +0100

    add withTrans HOC, add relative message id support

--- app/utils/i18n/Trans.ts
+++ app/utils/i18n/Trans.ts
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Context, PlainContext } from './format';
-import { renderToFragment } from './helper';
+import { contextualize, renderToFragment, withContext } from './helper';
 import { extractLocaleFromState } from './redux';
 import { translate } from './translate';
 
-type BoundTranslate = (id: string, context?: PlainContext) => string;
+export type BoundTranslate = (id: string, context?: PlainContext) => string;
 
 interface OwnProps extends Context {
   id?: string
@@ -16,16 +16,24 @@ interface Props extends OwnProps {
   locale: string
 }
 
-const Trans = ({ locale, id, markdown, children, ...context }: Props) => {
-  if (id) {
-    const translation = translate(locale, id, context, markdown);
-    return renderToFragment(translation);
-  } else if (typeof children === 'function') {
-    const boundTranslate: BoundTranslate = (innerId, innerContext) => translate(locale, innerId, { ...(context as PlainContext), ...innerContext }, false);
-    return renderToFragment(children(boundTranslate));
+const IdContext = React.createContext('');
+
+const Trans = contextualize(({ locale, id, markdown, children, ...context }: Props) => {
+  if (typeof children === 'function') {
+    const boundTranslate: BoundTranslate = (innerId, innerContext) =>
+      translate(locale, innerId.startsWith('.') ? `${id}${innerId}` : innerId, { ...(context as PlainContext), ...innerContext }, false);
+    const frag = renderToFragment(children(boundTranslate));
+    return id ? withContext(frag, IdContext, id) : frag;
+  } else if (id && !children) {
+    return renderTranslation(locale, id, context, markdown);
   }
   throw new Error('Do not nest React components in Trans component! Either pass a message id, or nest a callback taking the t function.');
-};
+}, IdContext, props => !props.id || props.id.startsWith('.'), (props, idFromContext) => ({ ...props, id: `${idFromContext}${props.id || ''}` }));
+
+function renderTranslation(locale: string, id: string, context: Context, markdown?: boolean) {
+  const translation = translate(locale, id, context, markdown);
+  return withContext(renderToFragment(translation), IdContext, id);
+}
 
 export default connect((state: any, props: OwnProps) => ({
   locale: extractLocaleFromState ? extractLocaleFromState(state) : 'unk'
--- app/utils/i18n/helper.ts
+++ app/utils/i18n/helper.ts
@@ -6,3 +6,14 @@ export function renderToFragment(reactNode: React.ReactNode, props?: React.Attri
   }
   return React.createElement(React.Fragment, props, reactNode);
 }
+
+export function withContext<T>(element: React.ReactElement<any>, context: React.Context<T>, value: T) {
+  return React.createElement(context.Provider, { value }, element);
+}
+
+export function contextualize<P, T>(component: React.ComponentType<P>, context: React.Context<T>, condition: (props: P) => boolean,
+                                    manipulator: (props: P, contextValue: T) => P) {
+  return (props: P) => condition(props)
+    ? React.createElement(context.Consumer, null, (valueFromContext: T) => React.createElement(component, manipulator(props, valueFromContext)))
+    : React.createElement(component, props);
+}
