961e352001cd1227cb0f48fafee6bc1abd7c5159
Matthias Piepkorn <mpiepk@gmail.com>
2023-03-04 17:30:38 +0100

    ♻️ use hash history for dev and prod

--- app/store/configureStore.prod.ts
+++ app/store/configureStore.prod.ts
@@ -1,14 +1,12 @@
 import { createStore, applyMiddleware, AnyAction } from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import eventMiddleware from './eventMiddleware';
 import createRootReducer from '../actions/index';
 import { RootState } from '../actions/types';
 
-const history = createBrowserHistory({
-  basename: window.location.pathname
-});
+const history = createHashHistory();
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk as ThunkMiddleware<RootState, AnyAction, void>, eventMiddleware, router);
 
