a6057111547cfd6bad1e1be515dec13d2536a6da
Matthias Piepkorn <mpiepk@gmail.com>
2023-12-26 18:25:59 +0100

    🔨 work around pnp bug in dev mode

--- internals/plugins/buildEnv.js
+++ internals/plugins/buildEnv.js
@@ -5,3 +5,13 @@ if (process.cwd().endsWith('nodegit') || process.cwd().endsWith('keytar')) {
     process.env.npm_config_target = '27.1.0';
     process.env.npm_config_disturl = 'https://electronjs.org/headers';
 }
+
+// workaround for PnP in dev mode
+const Module = require('node:module');
+const originalModuleLoad = Module._load;
+Module._load = function(request, parent, isMain) {
+  if (typeof parent === 'function') {
+    return originalModuleLoad.call(Module, request, undefined, isMain);
+  }
+  return originalModuleLoad.call(Module, request, parent, isMain);
+}
